cdcfb32661ae85472270f6b95d7d4ed98a911d56
jpred1972 <james.phipps@becloudit.com>
2017-01-29 12:33:33 -0600

    upload

---
+++ DjangoWebProject2/env/Scripts/pip3-script.py
@@ -0,0 +1,10 @@
+#!"c:\users\jamesphipps\documents\visual studio 2015\projects\djangowebproject2\djangowebproject2\env\scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==9.0.1','console_scripts','pip3'
+__requires__ = 'pip==9.0.1'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('pip==9.0.1', 'console_scripts', 'pip3')()
+    )
---
+++ DjangoWebProject2/env/Scripts/unidecode-script.py
@@ -0,0 +1,10 @@
+#!c:\users\jamesphipps\documents\visual studio 2015\Projects\DjangoWebProject2\DjangoWebProject2\env\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'Unidecode==0.4.19','console_scripts','unidecode'
+__requires__ = 'Unidecode==0.4.19'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('Unidecode==0.4.19', 'console_scripts', 'unidecode')()
+    )
